750937d19bddf6647d1178951b87eb1886e17689
Taipan <ocasiovictor69@gmail.com>
2026-05-17 18:21:12 -0400

    chore: install tsx locally as devDependency and purge compile folder

--- build_scripts/src/lib/video/logger.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.logStep = logStep;
-function logStep(step) {
-    var time = new Date().toISOString();
-    console.log("[".concat(time, "] \u25B6 ").concat(step));
-}
